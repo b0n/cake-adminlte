8bcfa1bcd20cf09b0bfb55f430c7376f1a6b979d
Kousuke Taekuchi <kousuketakeuchi@gmail.com>
2019-01-18 10:48:51 +0900

    add: plugins Adminlte

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,8 +2,6 @@
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
 
-Plugin::load('Gourmet/KnpMenu');
-
 Configure::write('AdminLTE', [
     'links' => [
         'logout' => null,
---
+++ src/Application.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Cirici\AdminLTE;
+
+use Cake\Core\Configure;
+use Cake\Error\Middleware\ErrorHandlerMiddleware;
+use Cake\Http\BaseApplication;
+use Cake\Routing\Middleware\AssetMiddleware;
+use Cake\Routing\Middleware\RoutingMiddleware;
+
+/**
+ * Application setup class.
+ *
+ * This defines the bootstrapping logic and middleware layers you
+ * want to use in your application.
+ */
+class Application extends BaseApplication
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function bootstrap()
+    {
+        $this->addPlugin('Gourmet/KnpMenu');
+
+        parent::bootstrap();
+    }
+    /**
+     * Setup the middleware queue your application will use.
+     *
+     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
+     * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
+     */
+    public function middleware($middlewareQueue)
+    {
+        $middlewareQueue
+            // Catch any exceptions in the lower layers,
+            // and make an error page/response
+            ->add(ErrorHandlerMiddleware::class)
+
+            // Handle plugin/theme assets like CakePHP normally does.
+            ->add(AssetMiddleware::class)
+
+            // Add routing middleware.
+            ->add(new RoutingMiddleware($this));
+
+        return $middlewareQueue;
+    }
+}
